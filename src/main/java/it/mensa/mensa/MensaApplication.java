9329a517c68a36fa319d7ee48a2d46f556a96b86
marioalberto93 <mariomazzoni0@gmail.com>
2023-06-24 17:38:38 +0200

    config

---
+++ src/main/java/it/mensa/mensa/MensaApplication.java
@@ -0,0 +1,13 @@
+package it.mensa.mensa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensaApplication.class, args);
+	}
+
+}
