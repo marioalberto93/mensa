defa1d1b92dc5c7a90fbcd6e0a123c70bfd72ccb
marioalberto93 <mariomazzoni0@gmail.com>
2023-06-24 18:08:08 +0200

    added UserModel

---
+++ src/main/java/it/mensa/mensa/model/User.java
@@ -0,0 +1,23 @@
+package it.mensa.mensa.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User{
+    public enum Usertype{
+        ADMIN, USER
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true)
+    private String username;
+    private String pass;
+    private Usertype usertype;
+}
