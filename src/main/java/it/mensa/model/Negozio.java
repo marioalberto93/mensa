29b9d990e26e479c18b70fc83e1bc1cc881a99e8
Mario <m.mazzoni@contrader.it>
2023-06-27 09:54:40 +0200

    aggiunto model negozio

---
+++ src/main/java/it/mensa/model/Negozio.java
@@ -0,0 +1,25 @@
+package it.mensa.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Negozio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String citta;
+    private String descrizione;
+    private String indirizzo;
+    private String nome;
+    private String provincia;
+    @OneToOne
+    private Anagrafica anagrafica;
+
+}
