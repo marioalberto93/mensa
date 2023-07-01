14f29954e63916275a58cdb26d85f2c316eddab2
marioalberto93 <mariomazzoni0@gmail.com>
2023-07-01 18:44:14 +0200

    User controller loginDTO

---
+++ src/main/java/it/mensa/controller/UserController.java
@@ -0,0 +1,21 @@
+package it.mensa.controller;
+
+import it.mensa.dto.LoginDTO;
+import it.mensa.dto.UserDTO;
+import it.mensa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController extends AbstractController<UserDTO>{
+
+    @Autowired
+    UserService service;
+
+    @GetMapping("/login")
+    public UserDTO login(@RequestBody LoginDTO loginDTO){
+        return service.findUserByUsernameAndPassword(loginDTO.getUsername(), loginDTO.getPassword());
+    }
+}
