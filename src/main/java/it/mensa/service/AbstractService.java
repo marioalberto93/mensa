72d4b0fe225354c9d15f34ae86b30a31522fccba
marioalberto93 <mariomazzoni0@gmail.com>
2023-07-01 18:10:00 +0200

    aggiunto serviceDTO e AbstractService

--- src/main/java/it/mensa/service/AbstractService.java
+++ src/main/java/it/mensa/service/AbstractService.java
@@ -1,4 +1,32 @@
 package it.mensa.service;
 
-public class AbstractService {
+import it.mensa.converter.Converter;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.swing.text.html.parser.Entity;
+
+public class AbstractService<DTO> implements ServiceDTO<DTO>{
+
+    CrudRepository<Entity, Long> crudRepository;
+    Converter<Entity, DTO> converter;
+    @Override
+    public DTO read(long id) {
+        return converter.toDTO(crudRepository.findById(id).get());
+    }
+    @Override
+    public DTO insert(DTO dto) {
+        return converter.toDTO(crudRepository.save(converter.toEntity(dto)));
+    }
+    @Override
+    public DTO update(DTO dto) {
+        return converter.toDTO(crudRepository.save(converter.toEntity(dto)));
+    }
+    @Override
+    public void delete(long id) {
+        crudRepository.deleteById(id);
+    }
+    @Override
+    public Iterable<DTO> getAll() {
+        return converter.toDTOList(crudRepository.findAll());
+    }
 }
--- src/main/java/it/mensa/service/Service.java
+++
@@ -1,4 +0,0 @@
-package it.mensa.service;
-
-public interface Service {
-}
